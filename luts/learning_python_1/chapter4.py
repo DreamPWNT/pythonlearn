0444d6b72e1c0da4ed606738179b524d83a5efaa
dream <dreampwnt@gmail.com>
2024-03-11 20:34:17 +0300

    add auto formatter

--- luts/learning_python_1/chapter4.py
+++ luts/learning_python_1/chapter4.py
@@ -8,9 +8,9 @@ print(math.pi)
 print(math.sqrt(69))
 
 print(random.random())
-print(random.choice([1,2,5,7,8,4,67,8]))
+print(random.choice([1, 2, 5, 7, 8, 4, 67, 8]))
 
-S = 'Spam'
+S = "Spam"
 
 print(len(S))
 print(S[0])
@@ -23,90 +23,86 @@ print(S[:-1])
 print(S[0:3])
 print(S[:])
 
-print(S + 'xyz')
+print(S + "xyz")
 print(S * 9)
 
-S = 'z' + S[1:]
+S = "z" + S[1:]
 
 print(S)
 
-T = 'shruberry'
+T = "shruberry"
 L = list(T)
 print(L)
-L[1] = 'c'
-print(''.join(L))
+L[1] = "c"
+print("".join(L))
 
-B = bytearray(b'spam')
+B = bytearray(b"spam")
 print(B)
-B.extend(b'eggs')
+B.extend(b"eggs")
 print(B)
 print(B.decode())
 
-S = 'Spam'
-print(S.find('pa'))
-print(S.find('pp'))
-print(S.replace('pa','XYZ'))
+S = "Spam"
+print(S.find("pa"))
+print(S.find("pp"))
+print(S.replace("pa", "XYZ"))
 
-line = 'aaa,bb,cccccc,ddddd'
-print(line.split(','))
+line = "aaa,bb,cccccc,ddddd"
+print(line.split(","))
 print(S.upper())
 print(S.isalpha())
 line = line + "    \n\n\n   \n\n "
 print(line)
-print(line.rstrip().split(','))
+print(line.rstrip().split(","))
 
 print(dir())
 
-print(S + 'NI!')
-print(S.__add__('NI!'))
+print(S + "NI!")
+print(S.__add__("NI!"))
 
-print('sp\xc4m')
-print(b'a\x01c')
-print(u'sp\u00c4m')
-print('sp\xc5\u00c4\U000000c4m')
-print('\u00A3')
-print('\u00A3'.encode('latin1'))
-print(b'\xA3'.decode('latin1'))
+print("sp\xc4m")
+print(b"a\x01c")
+print("sp\u00c4m")
+print("sp\xc5\u00c4\U000000c4m")
+print("\u00A3")
+print("\u00A3".encode("latin1"))
+print(b"\xA3".decode("latin1"))
 
-match = re.match('[/:](.*)[/:](.*)[/:](.*)','/usr/home:lumberjack')
+match = re.match("[/:](.*)[/:](.*)[/:](.*)", "/usr/home:lumberjack")
 print(match.groups())
 
-L = [123, 'spam', 1.23]
+L = [123, "spam", 1.23]
 print(L)
 print(L[0])
 print(L[:-1])
-print(L + [4,5,6])
+print(L + [4, 5, 6])
 print(L * 3)
 
-L.append('NI!')
+L.append("NI!")
 L.pop(2)
 
 print(L)
 
-M = ['bbb','zzz','aaa']
+M = ["bbb", "zzz", "aaa"]
 M.sort()
 print(M)
 M.reverse()
 print(M)
 
-M = [
-  [1,5,2],
-  [8,23,6],
-  [0,3,78]
-]
+M = [[1, 5, 2], [8, 23, 6], [0, 3, 78]]
 print(M)
 print(M[1])
 print(M[2][0])
 
 print([row[1] + 3 for row in M])
 print([row[0] for row in M if row[0] % 2 == 0])
-print([M[i][i] for i in [0,1,2]])
+print([M[i][i] for i in [0, 1, 2]])
 
-doubles = [c * 2 for c in 'spam']
+doubles = [c * 2 for c in "spam"]
 
 print(doubles)
 print(list(range(9)))
-print(list(range(-33,11,6)))
+print(list(range(-33, 11, 6)))
 print([[x + 13, x / 66] for x in range(6)])
 
 G = (sum(row) for row in M)
@@ -114,52 +110,54 @@ G = (sum(row) for row in M)
 print(next(G))
 print(next(G))
 print(next(G))
-print(list(map(sum,M)))
+print(list(map(sum, M)))
 print({sum(row) for row in M})
 print({i: sum(M[i]) for i in range(3)})
-print([ord(x) for x in 'spaam'])
-print({ord(x) for x in 'spaam'})
-print({x: ord(x) for x in 'spaam'})
+print([ord(x) for x in "spaam"])
+print({ord(x) for x in "spaam"})
+print({x: ord(x) for x in "spaam"})
 
-D = {'color':'pink', 'quantity': 4, 'food': 'Caesar salad'}
+D = {"color": "pink", "quantity": 4, "food": "Caesar salad"}
 
 print(D)
 
-D['quantity'] += 1
-D['color'] = 'red'
+D["quantity"] += 1
+D["color"] = "red"
 
 print(D)
 
 D2 = {}
-D2['name'] = 'Vasisualiy'
-D2['post'] = 'Senior JavaScript developer'
-D2['ages'] = 32
+D2["name"] = "Vasisualiy"
+D2["post"] = "Senior JavaScript developer"
+D2["ages"] = 32
 
 print(D2)
 
-D3 = dict(name='Vasisualiy', post='Senior JavaScript developer', ages=32)
+D3 = dict(name="Vasisualiy", post="Senior JavaScript developer", ages=32)
 print(D3)
-D4 = dict(zip(['name','post','ages'],['Vasisualiy','Senior JavaScript developer',32]))
+D4 = dict(
+    zip(["name", "post", "ages"], ["Vasisualiy", "Senior JavaScript developer", 32])
+)
 print(D4)
 
 D5 = {
-        'server_name': 'isapra-01-db',
-        'db-name': 'dt',
-        'roles': ['easapr','admin'],
-        'login': 'easapr',
-        'password': 'easapr1'
-     }
+    "server_name": "isapra-01-db",
+    "db-name": "dt",
+    "roles": ["easapr", "admin"],
+    "login": "easapr",
+    "password": "easapr1",
+}
 
 print(D5)
 
-D5['roles'].append('user')
+D5["roles"].append("user")
 
 print(D5)
-print('sss' in D5)
+print("sss" in D5)
 
-if not 'sss' in D5:
-  print('missingggg')
-  print('no key in D5 object')
+if not "sss" in D5:
+    print("missingggg")
+    print("no key in D5 object")
 
 Ks = list(D5.keys())
 print(Ks)
@@ -168,47 +166,47 @@ Ks.sort()
 print(Ks)
 
 for key in Ks:
-  print(key, '=>', D5[key])
+    print(key, "=>", D5[key])
 
-for char in 'spam':
-  print(char.upper())
-  
+for char in "spam":
+    print(char.upper())
+
 x = 4
 while x > 0:
-  print('spam' * x)
-  x -= 1
-  
-T = (1,2,3,4,5,4,3,4,56,3,65,3,23,5,6,2,2,6,7,6,2)
+    print("spam" * x)
+    x -= 1
+
+T = (1, 2, 3, 4, 5, 4, 3, 4, 56, 3, 65, 3, 23, 5, 6, 2, 2, 6, 7, 6, 2)
 
 print(len(T))
 print(T.index(6))
 print(T.count(2))
 print((2,) + T[13:])
 
-T2 = 11.11, 45, ['s','t','y'], 'Yeeeeep'
+T2 = 11.11, 45, ["s", "t", "y"], "Yeeeeep"
 print(T2)
 print(T2[2])
-T2[2].append('z')
+T2[2].append("z")
 print(T2)
 
-f = open('data.txt','w')
-f.write('Hello\n')
-f.write('world\n')
+f = open("data.txt", "w")
+f.write("Hello\n")
+f.write("world\n")
 f.close()
 
-f = open('data.txt','r')
+f = open("data.txt", "r")
 text = f.read()
 print(text)
 
-S = 'sp\xc4m'
-file = open('unidata.txt','w',encoding='utf-8')
+S = "sp\xc4m"
+file = open("unidata.txt", "w", encoding="utf-8")
 file.write(S)
 file.close()
-text = open('unidata.txt',encoding='utf-8').read()
+text = open("unidata.txt", encoding="utf-8").read()
 print(text)
 
-X = {'s','p','a','m'}
-Y = {'a','b','p','d'}
+X = {"s", "p", "a", "m"}
+Y = {"a", "b", "p", "d"}
 
 print(X & Y)
 print(X | Y)
@@ -216,14 +214,14 @@ print(X - Y)
 print(Y - X)
 print(X > Y)
 
-d = decimal.Decimal('3.14145')
+d = decimal.Decimal("3.14145")
 d = d + 1
 print(d)
 
-f = fractions.Fraction(2,3)
+f = fractions.Fraction(2, 3)
 f = f + 1
 print(f)
-f = f + fractions.Fraction(1,9)
+f = f + fractions.Fraction(1, 9)
 print(f)
 
 print(type(L))
--- luts/learning_python_1/chapter7.py
+++ luts/learning_python_1/chapter7.py
@@ -1,68 +1,68 @@
 import sys
 
-print('one' "two")
+print("one" "two")
 
-s = 'a\nb\tc'
+s = "a\nb\tc"
 
 print(s)
-print(r'c:\new\file1.txt')
-print('c:\\new\\file1.txt')
+print(r"c:\new\file1.txt")
+print("c:\\new\\file1.txt")
 
-mantra = '''Python is a 
+mantra = """Python is a 
             best languiage
-            in the world'''
+            in the world"""
 
 print(mantra)
-print(len('abc'))
-print('abc' + 'def')
-print('Ni!' * 6)
+print(len("abc"))
+print("abc" + "def")
+print("Ni!" * 6)
 
-myjob = 'hacker'
+myjob = "hacker"
 
 for c in myjob:
-  print(c, end='|')
+    print(c, end="|")
 
-print('k' in myjob)
-print('z' in myjob)
-print('spam' in 'abcdefspamkloijhy')
+print("k" in myjob)
+print("z" in myjob)
+print("spam" in "abcdefspamkloijhy")
 
-S = 'spam'
+S = "spam"
 
 print(S[0], S[-2])
 print(S[1:3], S[1:], S[:-1])
 
-S = 'abcdefghijklmnopqrstuvwxyz'
+S = "abcdefghijklmnopqrstuvwxyz"
 
 print(S[1:22:4])
 print(S[::3])
 print(S[::-1])
 
-print('spam'[1:3])
-print('spam'[slice(1,3)])
-print('spam'[::-1])
-print('spam'[slice(None,None,-1)])
-print(str('spam'),repr('spam'))
+print("spam"[1:3])
+print("spam"[slice(1, 3)])
+print("spam"[::-1])
+print("spam"[slice(None, None, -1)])
+print(str("spam"), repr("spam"))
 
 S = "42"
 
 print(int(S) + 128)
 print(S + str(33))
-print(ord('Z'))
+print(ord("Z"))
 print(chr(33))
 print(chr(10))
 
-for c in 'abcdefghijklmnopqrstuvwxyz':
-  print(f'Symbol {c} match ASCII code:', ord(c))
+for c in "abcdefghijklmnopqrstuvwxyz":
+    print(f"Symbol {c} match ASCII code:", ord(c))
 
-print('Factorio is a %s %s in the world' % ('best', 'game'))
-print('Factorio is a %d %s in the world' % (4, 'fun'))
+print("Factorio is a %s %s in the world" % ("best", "game"))
+print("Factorio is a %d %s in the world" % (4, "fun"))
 
-S = 'sjsjdfghalgbkm'
+S = "sjsjdfghalgbkm"
 
-print(S.replace('gh','zz'))
-print(S.find('zz'))
-print(S.find('bkm'))
-print('bkm' in S)
+print(S.replace("gh", "zz"))
+print(S.find("zz"))
+print(S.find("bkm"))
+print("bkm" in S)
 
 S2 = "God bless America"
 
@@ -76,34 +76,34 @@ S3 = S3.rstrip()
 
 print(S3.upper())
 print(S3.isalpha())
-print(S3.endswith('America'))
-print(S3.endswith('you'))
-print(S3.endswith('me'))
-print(S3.startswith('land'))
-print(S3.startswith('USA'))
-print(S3.startswith('This'))
+print(S3.endswith("America"))
+print(S3.endswith("you"))
+print(S3.endswith("me"))
+print(S3.startswith("land"))
+print(S3.startswith("USA"))
+print(S3.startswith("This"))
 
-print('that is %d %s bird!' % (1,'dead'))
+print("that is %d %s bird!" % (1, "dead"))
 
-exclamation = 'Ni'
+exclamation = "Ni"
 
-print('The knight who say %s!' % exclamation)
-print('%d %s %g you' % (1,'spam', 4.0))
-print('%s -- %s -- %s' % (42, 3.1415926, [1,2,3]))
+print("The knight who say %s!" % exclamation)
+print("%d %s %g you" % (1, "spam", 4.0))
+print("%s -- %s -- %s" % (42, 3.1415926, [1, 2, 3]))
 
 x = 1234
 
-print('integers: ...%d...%-6d...%06d' % (x,x,x))
+print("integers: ...%d...%-6d...%06d" % (x, x, x))
 
 x = 1.23456789
 
-print('%e | %f | %g' % (x,x,x))
-print('%-6.2f | %05.2f | %+06.1f' % (x,x,x))
-print('%s' % x, str(x))
-print('%f, %.2f, %.*f' % (1/3.0, 1/3.0, 10, 1/3.0))
+print("%e | %f | %g" % (x, x, x))
+print("%-6.2f | %05.2f | %+06.1f" % (x, x, x))
+print("%s" % x, str(x))
+print("%f, %.2f, %.*f" % (1 / 3.0, 1 / 3.0, 10, 1 / 3.0))
 
-D = {'qty': 1, 'food': 'spam'}
-print('%(qty)d more %(food)s' % D)
+D = {"qty": 1, "food": "spam"}
+print("%(qty)d more %(food)s" % D)
 
 values = {"db_name": "dt", "encoding": "utf"}
 reply = """I want to use 
@@ -113,37 +113,42 @@ in %(encoding)s encoding
 
 print(reply % values)
 print(vars())
-print('My job is %(myjob)s, a thik that %(S2)s and my favorite db is: %(values)s' % vars())
+print(
+    "My job is %(myjob)s, a thik that %(S2)s and my favorite db is: %(values)s" % vars()
+)
 
-template = '{0}, {1} and {2}'
+template = "{0}, {1} and {2}"
 
-print(template.format('spam','ham','eggs'))
+print(template.format("spam", "ham", "eggs"))
 
-template = '{motto}, {pork} and {food}'
+template = "{motto}, {pork} and {food}"
 
-print(template.format(motto='spam', pork='ham', food='eggs'))
+print(template.format(motto="spam", pork="ham", food="eggs"))
 
-template = '{motto}, {0} and {food}'
+template = "{motto}, {0} and {food}"
 
-print(template.format('ham', motto='spam', food='eggs'))
+print(template.format("ham", motto="spam", food="eggs"))
 
-template = '{}, {} and {}'
+template = "{}, {} and {}"
 
-print(template.format('spam','ham','eggs'))
+print(template.format("spam", "ham", "eggs"))
 
-print('My mega {1[kind]} runs on {0.platform}'.format(sys, {'kind': 'computer'}))
+print("My mega {1[kind]} runs on {0.platform}".format(sys, {"kind": "computer"}))
 
-somelist = list('SPAM')
+somelist = list("SPAM")
 
-print('first={0[0]}, third={0[2]}'.format(somelist))
-print('first={0}, last={1}'.format(somelist[0], somelist[-1]))
+print("first={0[0]}, third={0[2]}".format(somelist))
+print("first={0}, last={1}".format(somelist[0], somelist[-1]))
 
 parts = somelist[0], somelist[-1], somelist[1:3]
 
-print('first={0}, last={1}, middle={2}'.format(*parts))
-print('{0:10} = {1:10}'.format('spam', 123.4567))
-print('{0:>10} = {1:<10}'.format('spam', 123.4567))
-print('{0.platform:>10} = {1[kind]:<10}'.format(sys, dict(kind='laptop')))
-print('{0:e}, {1:.3e}, {2:g}'.format(3.14159, 3.14159, 3.14159))
-print('{0:f}, {1:.2f}, {2:06.2f}'.format(3.14159, 3.14159, 3.14159))
-print('{0:X}, {1:o}, {2:b}'.format(255,255,255))
+print("first={0}, last={1}, middle={2}".format(*parts))
+print("{0:10} = {1:10}".format("spam", 123.4567))
+print("{0:>10} = {1:<10}".format("spam", 123.4567))
+print("{0.platform:>10} = {1[kind]:<10}".format(sys, dict(kind="laptop")))
+print("{0:e}, {1:.3e}, {2:g}".format(3.14159, 3.14159, 3.14159))
+print("{0:f}, {1:.2f}, {2:06.2f}".format(3.14159, 3.14159, 3.14159))
+print("{0:X}, {1:o}, {2:b}".format(255, 255, 255))
+
+a, b, c = input().split()
+print(a.ljust(3, "0") + "\n" + b.ljust(3, "0") + "\n" + c.ljust(c, "0"))

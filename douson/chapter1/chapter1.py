3efad628ff59ebcb4167f5bc091341eaaed80fcc
dream <dreampwnt@gmail.com>
2025-01-23 00:29:33 +0300

    Douson 1-2 chapter finished

---
+++ douson/chapter1/chapter1.py
@@ -0,0 +1,13 @@
+# Game Over
+# Демонстрирует работу функции print
+
+print("Game Over")
+input("\n\nНажмите Enter, чтобы выйти.")
+
+# HW
+
+print("Dmitry")
+input("\n\nНажмите Enter, чтобы выйти.")
+
+print("Я знаю пароль, я вижу ориентир. Я верю только в это Medoff спасёт мир")
+print("Долганов Д.В.")

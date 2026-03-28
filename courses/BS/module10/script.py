572f780f75ef16f32219aa79b95333686bbf9e8b
DreamPWNT <dimon_33@mail.ru>
2026-03-28 19:52:50 +0300

    Bogdan module 10 finished

---
+++ courses/BS/module10/script.py
@@ -0,0 +1,19 @@
+my_number = 10
+
+print(id(my_number))
+
+my_string = 'abc'
+
+print(id(my_string))
+
+my_name = 'Vasisualiy'
+
+print(id(my_name))
+
+my_num = 100
+
+print(id(my_num))
+
+other_num = my_num
+
+print(id(other_num))

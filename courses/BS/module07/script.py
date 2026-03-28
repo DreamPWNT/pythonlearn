33ec71cf3a2a4cd807fc948f98354b5757be6600
DreamPWNT <dimon_33@mail.ru>
2026-03-28 18:55:15 +0300

    Bogdan module 7 finish

---
+++ courses/BS/module07/script.py
@@ -0,0 +1,23 @@
+def hello(name):
+    print('Hello there,', name)
+    print('Hi there,', name)
+
+
+print('Outside of the fn')
+
+hello('Vasisualiy')
+hello('Evlampiy')
+
+
+def sum_nums(a, b):
+    sum = a + b
+
+    return sum
+
+
+first_sum = sum_nums(10, 5)
+
+print(first_sum)
+print(sum_nums(50.5, 20))
+
+print(sum_nums(sum_nums(50.5, 20), 30))

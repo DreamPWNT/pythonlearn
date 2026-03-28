375a1c14d419ee7cabe27021837226dd224f830e
DreamPWNT <dimon_33@mail.ru>
2026-03-28 17:53:45 +0300

    Bogdan module 5-6

---
+++ courses/BS/module05/script.py
@@ -0,0 +1,27 @@
+print(print)
+
+name = input("Enter your name: ")
+
+print(name)
+
+print(dir(name))
+print(name.upper())
+print(10, "Vasisualiy", True)
+print(dir(__builtins__))
+
+print(name.upper())
+print(name.count("a"))
+
+
+def my_name(name):
+    print(name)
+
+
+a = 10
+
+
+def my_fn():
+    print('Hello')
+
+
+my_name("Vasisualy!!!!")

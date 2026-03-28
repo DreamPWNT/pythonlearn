dd5dd898e076cd01267826cce59253b1f436d452
DreamPWNT <dimon_33@mail.ru>
2026-03-28 20:42:22 +0300

    Bogdan module 12 finished

---
+++ courses/BS/module12/script.py
@@ -0,0 +1,67 @@
+friends_quantity = 50
+
+print(friends_quantity)
+print(type(friends_quantity))
+
+any_num = int(input("Enter any number: "))
+
+print(any_num)
+print(type(any_num))
+
+base = 5
+power = 3
+
+print(pow(base, power))
+print(type(pow(5, 3)))
+
+one_million = 1_000_000
+
+print(one_million)
+
+my_number = 3_427
+
+print(my_number)
+
+input_str = input("Enter any number:   ")
+input_int = int(input_str)
+
+print(input_int)
+print(type(input_int))
+
+long_int = 1_000_000_000
+
+print(long_int)
+
+average_price = 17.25
+
+print(average_price)
+print(type(average_price))
+
+price = int(average_price)
+
+print(price)
+print(type(price))
+
+str_temperature = '14.5'
+temperature = float(str_temperature)
+
+print(temperature)
+print(type(temperature))
+print(round(average_price))
+
+rate = 0.78
+
+print(round(rate))
+print(type(round(rate)))
+
+complex_a = 3 + 5j
+complex_b = 4 + 7j
+
+sum = complex_a + complex_b
+
+print(sum)
+print(type(sum))
+
+print(complex_a - complex_b)
+print(complex_a * complex_b)
+print(complex_a / complex_b)

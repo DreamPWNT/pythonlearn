6fc516d60d7d08a498a52ab4b6b40c57c7757615
dream <dimon_33@mail.ru>
2026-04-08 17:38:51 +0300

    continue task book chapter 2

--- task_book/chapter02/task51.py
+++ task_book/chapter02/task51.py
@@ -1,5 +1,20 @@
+import math
+
 a = float(input('Enter "a" coefficient of square equation: '))
 b = float(input('Enter "b" coefficient of square equation: '))
 c = float(input('Enter "c" coefficient of square equation: '))
 
 D = pow(b, 2) - 4 * a * c
+
+if D < 0:
+    print(f'Square equation {a}x^2+({b}x)+({c}) = 0 has no roots')
+elif D == 0:
+    x = -b / (2 * a)
+
+    print(f'Square equation {a}x^2+({b}x)+({c}) = 0 has one root. x = {x}')
+else:
+    x1 = (-b + math.sqrt(D)) / (2 * a)
+    x2 = (-b - math.sqrt(D)) / (2 * a)
+
+    print(
+        f'Square equation {a}x^2+({b}x)+({c}) = 0 has two roots. x1 = {x1}, x2 = {x2}')

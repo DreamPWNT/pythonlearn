824b2a9220b2d37af50ff2cbcb61409a692e642b
dream <dimon_33@mail.ru>
2026-04-16 18:04:37 +0300

    Luts finish chapter 12

--- luts/2026/chapter12/script.py
+++ luts/2026/chapter12/script.py
@@ -39,3 +39,99 @@ elif choice == 'Windows':
     print(1985)
 else:
     print('Bad choice')
+
+branch = dict(macos=2001, Linux=1991, Windows=1985)
+
+print(branch.get('Windows', 'Bad choice'))
+print(branch.get('Solaris', 'Bad choice'))
+
+choice = 'AmigaOS'
+
+if choice in branch:
+    print(branch[choice])
+else:
+    print('Bad choice')
+
+choice = 'GEM'
+
+try:
+    print(branch[choice])
+except KeyError:
+    print('Bad choice')
+
+state = 'halt'
+
+match state:
+    case 'go':
+        print('green')
+    case 'stop':
+        print('red')
+    case _:
+        print('yellow')
+
+match state:
+    case 'go' | 'proceed' | 'start':
+        print('green')
+    case 'stop' | 'halt' as what:
+        print('red')
+        print('means', what)
+    case other:
+        print('catchall', other)
+
+for stmt in ['if', 'while', 'try']:
+    match stmt:
+        case 'if' | 'match':
+            print('logic')
+        case 'for' | 'while' as which:
+            print('loop')
+        case other:
+            print('tdb')
+
+print(which, other)
+
+state = dict(a=0, values=[1, 2, 3], tr=False, name='Vasisualiy')
+
+print('\n\n')
+
+match state:
+    case 1 | 2 | 3 as what:
+        print('or', what)
+    case [1, 2, what]:
+        print('list', what)
+    case [0, *what]:
+        print('list', what)
+    case {'a': 1, 'b': 2, 'c': what}:
+        print('dict', what)
+    case {'a': 0, **what}:
+        print('dict', what)
+    case (2, 3, what):
+        print('tuple, what')
+    case (0, *what):
+        print('tuple', what)
+    case _ as what:
+        print('other', what)
+
+
+class Emp:
+    def __init__(self, name): self.name = name
+
+
+pat = Emp('Pat')                         # pat.name becomes 'Pat': see Part VI!
+
+state = pat
+
+match state:
+    case pat.name as what:               # Match object's attribute, what = 'Pat'
+        print('attr', what)
+    case Emp(name=what):                 # Match an Emp instance, what = 'Pat'
+        print('instance', what)
+
+tone = 'formal'
+
+a = 'code' if tone == 'formal' else 'hack'
+
+print(tone, a)
+
+L = [1, 0, 2, 0, 'hack', '', 'py', []]
+
+print(list(filter(bool, L)))

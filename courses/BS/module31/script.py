6fa47f03c9644050625fa87808adff293f749971
dream <dimon_33@mail.ru>
2026-04-20 15:10:11 +0300

    Bogdan module 31 finished

---
+++ courses/BS/module31/script.py
@@ -0,0 +1,26 @@
+hello = 'Hello'
+world = 'World'
+
+greeting = hello + ' ' + world
+
+print(greeting)
+
+greeting = f'{hello} {world}'
+
+print(greeting)
+
+my_name = 'Vasisualiy'
+my_hobby = 'running'
+time = 8
+
+info = my_name + ' likes ' + my_hobby + ' at ' + str(time) + ' o\'clock'
+
+print(info)
+
+info = f'{my_name} likes {my_hobby} at {time} o\'clock'
+
+print(info)
+
+info = f'{my_name} likes {my_hobby} at {time} o\'clock'
+
+print(info.title())

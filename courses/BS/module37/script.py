8a57f56a7cf28adcb85064d12cd23e1994c633d5
DreamPWNT <dimon_33@mail.ru>
2026-04-22 11:08:51 +0300

    Bogan finished 37 module

--- courses/BS/module37/script.py
+++ courses/BS/module37/script.py
@@ -36,3 +36,42 @@ for num in range(5):
 
 for odd_num in range(3, 10, 2):
     print(odd_num)
+my_list = [True, 10, 'abc', {}]
+
+for elem in my_list:
+    print(elem)
+
+video_info = ('1920x1080', True, 27)
+
+for elem in video_info:
+    print(elem)
+
+my_object = {
+    'x': 10,
+    'y': True,
+    'z': 'abc'
+}
+
+for key in my_object:
+    print(key, my_object[key])
+
+for el in [1, 'abc', True]:
+    print(type(el))
+    print(el)
+
+my_dict = {'id': 34, 'title': 'test'}
+
+for key in my_dict:
+    print(type(key))
+    print(key)
+    print(my_dict[key])
+
+my_object = {
+    'x': 10,
+    'y': True
+}
+
+for item in my_object.items():
+    key, value = item
+
+    print(repr((key, value)))
